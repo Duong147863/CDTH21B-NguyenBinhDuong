8e3e4d1bd83b9625987efa370f1edc36c27c823e
Duong147863 <duong147863@gmail.com>
2023-02-24 22:25:20 +0700

    CDTH21B-NguyenBinhDuong

---
+++ 0306211127.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<iostream>
+using namespace std;
+
+int
+main ()
+{
+  int n, a, b;
+  cout << "nhap so co 2 chu so ";
+  cin >> n;
+  a = n / 10;
+  b = n % 10;
+  cout << "so hang chuc =" << a << endl;
+  cout << "so hang dvi =" << b;
+
+  return 0;
+}
